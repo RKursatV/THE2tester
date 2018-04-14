5da5237e5e0e5b31c27eedbf571ca627a977d7de
zararli <zararli@users.noreply.github.com>
2018-04-14 22:31:26 +0300

    Update tester.py

--- tester.py
+++ tester.py
@@ -38,9 +38,12 @@ def show_message(message, text_type=bcolors.ENDC, clear_before=0,line=0,tab=1,sp
 #converts given parameter to formatted querystring value and opens by default browser of the os
 def show_solution(question):
 		question = urllib.pathname2url(question)
-		Popen(['python -m webbrowser \'' + "https://www.webqc.org/balance.php?reaction=" + question + '\''], stderr=PIPE, stdout=PIPE, shell=True)
-		Popen(['python -m webbrowser \'' + "https://www.wolframalpha.com/input/?i=" + question + '\''], stderr=PIPE, stdout=PIPE, shell=True)
-
+		a=Popen("python -m webbrowser " + "https://www.webqc.org/balance.php?reaction=" + question, stderr=PIPE, stdout=PIPE, shell=True)
+		b=Popen("python -m webbrowser " + "https://www.wolframalpha.com/input/?i=" + question, stderr=PIPE, stdout=PIPE, shell=True)
+		out, error = a.communicate()
+		out2, error2 = b.communicate()
+		print out,error,out2,error2
+		exit()
 #sample format: [<unbalanced equation>, <coefficients of its balanced version>]
 samples = [
 		["H3PO4 + (NH4)2MoO4 + HNO3 -> (NH4)3PO4(MoO3)12 + NH4NO3 + H2O", "1 12 21 1 21 12"], 
@@ -64,25 +67,37 @@ samples = [
 		["B2Br6 + HNO3 -> B(NO3)3 + HBr", "1 6 2 6"],
 		["NH4OH + KAl(SO4)2 + 12H2O -> Al(OH)3 + (NH4)2SO4 + KOH + H2O", "4 1 1 2 1 12"],
 		["C2H6O + O2 -> CO2 + H2O", "1 3 2 3"],
-		["Fe + HC2H3O2 -> Fe(C2H3O2)3 + H2", "2 6 2 3"],
-
-		["3 4 5", "3 4 5"]  
+		["Fe + HC2H3O2 -> Fe(C2H3O2)3 + H2", "2 6 2 3"]
 		]
 prepare_screen()
 #checks whether the source is available or not
 if (os.path.isfile("the2.c") != True):
 	show_message("Did you forget to put your the2.c file to this folder?",bcolors.FAIL)
 	show_message("I can't believe you!",bcolors.FAIL)
-	show_message("Exiting...",bcolors.FAIL)
+	show_message("Exiting..." + bcolors.ENDC,bcolors.FAIL)
 	show_message("Hint: " + bcolors.ENDC + "copy your the2.c file to " + bcolors.WARNING + os.path.dirname(os.path.realpath(__file__)) + bcolors.ENDC + " directory then run me again",bcolors.OKBLUE,0,2,1,1)
 	exit()
 #removes compiled file if exists to avoid problems
 if (os.path.isfile("the2") == True):
 	os.remove("the2")
+if (os.path.isfile("the2.exe") == True):
+	os.remove("the2.exe")
 #compiles file using gcc
-p=Popen(['gcc', '-Wall', '-ansi', '-pedantic-errors', '-lm', '-o', 'the2', 'the2.c'], stdout=PIPE, stderr=PIPE)
+try:
+	p=Popen(['gcc', '-Wall', '-ansi', '-pedantic-errors', '-lm', '-o', 'the2', 'the2.c'], stdout=PIPE, stderr=PIPE)
+except Exception as e:
+	show_message("It seems gcc is not installed.",bcolors.FAIL)
+	show_message("I can't work under these conditions",bcolors.FAIL)
+
+	show_message("PS: " + bcolors.ENDC + "Feel free to contact me. I would be happy to help you",bcolors.OKBLUE,0,1,1,1)
+	show_message("Exiting..." + bcolors.ENDC,bcolors.FAIL)
+	exit()
+else:
+	pass
 out, error = p.communicate()
-if out != None or error != None:
+print out, error
+
+if (out != None or error != None) and (len(out) > 0 or len(error) > 0):
 	prepare_screen()
 	show_message("It seems your code having some trouble at compile time.", bcolors.WARNING , 0, 0, 1, 0)
 	show_message("1) Stop working and give me some time, I'll be back soon", bcolors.ENDC , 0, 0, 1, 0)
@@ -91,13 +106,15 @@ if out != None or error != None:
 	answer = raw_input("\tAnswer: ")
 	if answer and answer.isdigit():
 		if int(answer) == 1:
-			show_message("Exiting... I believe you, you can do it!",bcolors.FAIL,0,2,1,1)
+			prepare_screen()
+			show_message("Exiting... I believe you, you can do it!" + bcolors.ENDC,bcolors.FAIL,0,2,1,1)
 			exit()
 		elif int(answer) == 3:
-			show_message("The fine problem is",bcolors.FAIL,0,0,1,0)
+			prepare_screen()
+			show_message("The fine problem is",bcolors.WARNING,0,0,1,0)
 			show_message(out,bcolors.ENDC,0,0,2,0)
 			if error: show_message(error,bcolors.ENDC,0,0,1,0)
-			show_message("Exiting... Good luck with the problem.",bcolors.FAIL,0,2,1,1)
+			show_message("Exiting... Good luck with the problem." + bcolors.ENDC,bcolors.FAIL,0,2,1,1)
 			exit()
 prepare_screen()
 type = 0
@@ -115,8 +132,8 @@ if(answer):
 			show_message("We're going to inspect each case carefully",bcolors.HEADER)
 			type = 1
 		elif int(answer) == 3:
-			show_message("I cannot understand you humans",bcolors.FAIL,0,2,1,1)
-			show_message("Exiting...",bcolors.FAIL,0,2,1,0)
+			show_message("I cannot understand you humans",bcolors.FAIL,0,1,1,0)
+			show_message("Exiting..." + bcolors.ENDC,bcolors.FAIL,0,1,1,0)
 			exit()
 		elif int(answer) == 2:
 			show_message("Sometimes the questions are complicated and the answers are simple.",bcolors.HEADER,0,1,1)
@@ -126,7 +143,7 @@ if(answer):
 	if(answer.isdigit() != True or bad_ent):
 		show_message("Are you bored? Wanna have some fun?",bcolors.HEADER)
 		show_message("Consider visiting " + bcolors.ENDC + "http://www.theuselessweb.com", bcolors.HEADER)
-		show_message("Exiting...",bcolors.FAIL,0,2,1,1)
+		show_message("Exiting..." + bcolors.ENDC,bcolors.FAIL,0,2,1,1)
 		exit()
 else:
 	show_message("We're going to inpect each case carefully",bcolors.HEADER)
@@ -140,7 +157,14 @@ while index < total:
 	show_message("<Testing Equation " + bcolors.BOLD + bcolors.UNDERLINE + str(index) + bcolors.ENDC +"> ",bcolors.WARNING)
 	show_message("="*50,bcolors.HEADER)
 	input = smp[0]
-	cproc = Popen("./the2",stdin=PIPE, stdout=PIPE)
+	try:
+		cproc = Popen("./the2",stdin=PIPE, stdout=PIPE)
+	except Exception as e:
+		prepare_screen()
+		show_message("It seems your code had critical problems while compiling. No compiled file is generated.", bcolors.WARNING , 0, 0, 1, 0)
+		show_message("I can't work without the binary file.", bcolors.WARNING , 0, 0, 1, 0)
+		show_message("Exiting..." + bcolors.ENDC,bcolors.FAIL,0,1,1,1)
+		exit()
 	start_time = time.time()
 	out, error = cproc.communicate(input)
 	elapsed_time = str(time.time() - start_time)
@@ -151,7 +175,6 @@ while index < total:
 	if(out != smp[1]):
 		show_message("Your output",bcolors.OKBLUE,0,0,1,0)
 		show_message(out,bcolors.ENDC,0,0,2,0)
-		error = "as"
 		if error is not None:
 			show_message("Runtime Error",bcolors.FAIL,0,0,1,0)
 			show_message(error, bcolors.ENDC,0,0,2,0)			
@@ -183,7 +206,8 @@ while index < total:
 					show_solution(smp[0])
 					index -= 1
 				elif int(answer) == 3:
-					show_message("Exiting...",bcolors.FAIL,0,2,1,1)
+					show_message("Exiting..." + bcolors.ENDC,bcolors.FAIL,0,2,1,1)
 					exit()
 
 	index += 1
+show_message("Exiting..." + bcolors.ENDC,bcolors.FAIL,0,2,1,1)
